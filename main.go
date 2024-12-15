799b3c2e36665f6bf75bc51df388d9a404cbcd5c
Kerntarn <fluk032548@gmail.com>
2024-12-15 22:36:02 +0700

    add: simple CRUD

--- main.go
+++ main.go
@@ -1,44 +1,8 @@
 package main
 
 import (
-	"strconv"
-
 	"github.com/gofiber/fiber/v2"
 )
-
-type Book struct {
-	ID     int    `json:"id"`
-	Title  string `json:"title"`
-	Author string `json:"author"`
-}
-
-var books []Book
-
-func getBooks(c *fiber.Ctx) error {
-	return c.JSON(books)
-}
-func getBook(c *fiber.Ctx) error {
-	bookId, err := strconv.Atoi(c.Params("id"))
-	if err != nil {
-		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
-	}
-	for _, book := range books {
-		if book.ID == bookId {
-			return c.JSON(book)
-		}
-	}
-	return c.SendStatus(fiber.StatusNotFound)
-}
-
-func createBook(c *fiber.Ctx) error {
-	book := new(Book)
-
-	if err := c.BodyParser(book); err != nil {
-		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
-	}
-	books = append(books, *book)
-	return c.JSON(book)
-}
 
 func main() {
 	app := fiber.New()
@@ -49,6 +13,8 @@ func main() {
 	app.Get("/books", getBooks)
 	app.Get("/books/:id", getBook)
 	app.Post("/books", createBook)
+	app.Put("/books/:id", updateBook)
+	app.Delete("/books/:id", deleteBook)
 
 	app.Listen(":3000")
 }
